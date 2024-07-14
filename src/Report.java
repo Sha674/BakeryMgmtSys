bac54a45c63194b482fc86fa386c89dd45a51733
Shannon <shanonntan@gmail.com>
2024-07-14 23:17:46 +0800

    new file:   src/Report.java

---
+++ src/Report.java
@@ -0,0 +1,18 @@
+import java.sql.Date;
+
+public class Report {
+    private String report_id;
+    private Date report_date;
+    
+    public Report(){
+
+    }
+
+    public void generateSalesRp(){
+
+    }
+
+    public void generateInventoryRp(){}
+
+    public void generateOrderRp(){}
+}
