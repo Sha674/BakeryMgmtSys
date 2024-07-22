064f13f95cc80b19eedbd9012cdaf02b3cf5f203
imlmy <liewmeiyan0914@gmail.com>
2024-07-23 03:44:09 +0800

    Update Order.java

--- src/Order.java
+++ src/Order.java
@@ -1,66 +1,78 @@
 import java.util.*;
+import java.time.LocalDate;
 
 public class Order {
     private String order_id;
     private Date order_date;
-    private Customer cust;
-    private ArrayList<Items> menu_items;
-    // private ArrayList<Integer> qty;
+    //private Customer cust;
+    private ArrayList<OrderedItem> ordered_items;
     private String order_status;
     private String payment_type;
 
-    public Order(){
-    }
-
-    public Order(Customer cust, ArrayList<Items> menu_items, String payment_type){
-        // order_id=
-        // order_date=
-        this.cust=cust;
-        this.menu_items=menu_items;
-        order_status="Pending";
-        this.payment_type=payment_type;
-
-    }
-
-    public void setOrder(Customer cust, ArrayList<Items> menu_items, String payment_type)
+    public Order()
     {
-        this.cust=cust;
-        this.menu_items=menu_items;
+        order_id = UUID.randomUUID().toString();
+        order_date = new Date();
+        ordered_items = new ArrayList<OrderedItem>();
+        order_status = "Pending";
+        order_id = UUID.randomUUID().toString();
+        payment_type = " ";
+    }
+
+    public Order(ArrayList<OrderedItem> ordered_items, String payment_type)
+    {
+        //this.cust=cust;
+        this.ordered_items = ordered_items;
+        this.payment_type=payment_type;
+        order_id = UUID.randomUUID().toString();
+        order_date = new Date();
+        order_status="Pending";
+    }
+    
+    public void setOrder(ArrayList<OrderedItem> ordered_items, String payment_type)
+    {
+        //this.cust=cust;
+        this.ordered_items = ordered_items;
         this.payment_type=payment_type;
     }
+    
+    public void updateStatus(order_status)
+    {
+        this.order_status = order_status;
+    }
+    
 
     public String getOrderId()
     {
         return order_id;
     }
     
-    public ArrayList<Items> getMenus(){
-        return menu_items;
-    }
-
-    // public String getDetails(boolean flag,Items menuItems){//to generate sales report
-    //     if(flag){
-    //         return String.format("%-8s %-7s %-18s %-15s ", order_date,order_id,cust.getName(),payment_type,menuItems.getName(),menuItems.getPrice(),qty);
-    //     }
-    //     else{
-    //         return String.format("%-8s %-7s %-18s %-15s ", order_date,order_id,cust.getName(),payment_type);
-    //     }
-    // }
-
-    public void updateStatus(String order_status)
+    public ArrayList<OrderedItem> ordered_items()
     {
-        this.order_status=order_status;
+        return ordered_items;
     }
-
-    // public double calculateTotalPrice()
-    // {
-
-    // }
-
-    // public void displayOrder()
-    // {
-    //     System.out.println();
-    // }
-
     
+    public double calculateTotalPrice()
+    {
+        double totalPrice = 0;
+        for (OrderedItem orderedItem : ordered_items) {
+            totalPrice += orderedItem.getAmount();
+        }
+        return totalPrice;
+    }
+    
+    public void displayOrder()
+    {
+        System.out.println("Order Id: "+order_id);
+        System.out.println("Order Date: "+order_date);
+        System.out.println("Order Status: "+order_status);
+        System.out.println("Payment Type: " + payment_type);
+        System.out.println("Ordered Items: ");
+        for(OrderedItem o:ordered_items)
+		{
+		    System.out.println(o);
+		}
+		System.out.println("Total Price: RM" + calculateTotalPrice());
+        
+    }
 }
