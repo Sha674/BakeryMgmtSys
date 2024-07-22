9d22b5747a6ef874941923e6131be08d1a099e7b
Shannon <shanonntan@gmail.com>
2024-07-22 17:29:10 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/BakeryMgmtSys.java
    	modified:   src/User.java

--- src/BakeryMgmtSys.java
+++ src/BakeryMgmtSys.java
@@ -1,12 +1,25 @@
 import java.util.*;
 public class BakeryMgmtSys 
 {
-    public static void main(String[] args) throws Exception 
+    public static void loginPage()
     {
-        Scanner sc = new Scanner(System.in);
-        String uName,pass;
-        int choose;
-        boolean flag = true;
+        Scanner sc=new Scanner(System.in);
+        int select;
+        System.out.println("Login Page");
+        System.out.println("1.Admin Login");
+        System.out.println("2.Customer Login");
+        select = sc.nextInt();
+        // switch(select)
+        // {
+        //     case 1:
+        // }
+    }
+    public static void main(String[] args) throws Exception 
+    { 
+        Scanner sc=new Scanner(System.in);
+        // String uName,pass;
+         int choose;
+        // boolean flag = true;
         Customer c = new Customer();
         System.out.println("Bakery Management System");
         System.out.println("1.Login");
@@ -16,9 +29,7 @@ public class BakeryMgmtSys
         switch(choose)
         {
             case 1:System.out.println("hello");break;
-            case 2:c.registerPage();
-                    
-                    break;
+            case 2:c.registerPage();break;
             case 3:System.out.println("hello");break;
             default:System.out.println("Please try again");break;
         }
--- src/User.java
+++ src/User.java
@@ -1,3 +1,4 @@
+
 public class User {
     protected String id;
     protected String password;
@@ -21,6 +22,15 @@ public class User {
     public void setPw(String password)
     {
         this.password=password;
+    }
+
+    public void loginPage()
+    {
+        int choose;
+        System.out.println("Login Page");
+        System.out.println("1.Admin Login");
+        System.out.println("2.Customer Login");
+
     }
 
     public boolean login(String id, String password)
