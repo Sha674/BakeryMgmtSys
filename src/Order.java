5d8c564fd51b4cd1de6fe3d844eb8b5f3e425870
Shannon <shanonntan@gmail.com>
2024-07-24 00:40:52 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/Order.java

--- src/Order.java
+++ src/Order.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Order {
     private String order_id;
     private Date order_date;
-    private String cusId;
+    private String cusName;
     private ArrayList<OrderedItem> ordered_items;
     //private ArrayList<Integer> quantity;
     private String order_status;
@@ -14,17 +14,17 @@ public class Order {
         //quantity=new ArrayList<Integer>();
     }
 
-    public Order(String cusId, ArrayList<OrderedItem> ordered_items, String payment_type){
+    public Order(String cusName, ArrayList<OrderedItem> ordered_items, String payment_type){
         this();
         order_id = UUID.randomUUID().toString();
         order_date = new Date();
         order_status="Pending";
-        setOrder(cusId,ordered_items,payment_type);
+        setOrder(cusName,ordered_items,payment_type);
     }
 
-    public void setOrder(String cusId,ArrayList<OrderedItem> ordered_items,String payment_type)
+    public void setOrder(String cusName,ArrayList<OrderedItem> ordered_items,String payment_type)
     {
-        this.cusId=cusId;
+        this.cusName=cusName;
         this.ordered_items = ordered_items;
         this.payment_type=payment_type;
     }
@@ -72,6 +72,7 @@ public class Order {
         System.out.println("-------------------------------------------------------------------------------------------");
         System.out.println("Order Id: "+order_id);
         System.out.println("Order Date: "+order_date);
+        System.out.println("Customer name: "+cusName);
         System.out.println("Order Status: "+order_status);
         System.out.println("Payment Type: " + payment_type);
         System.out.println("\nOrdered Items: ");
